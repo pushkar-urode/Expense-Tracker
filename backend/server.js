8fe5c8545885332150afbcf9293c49549b98889d
Dark Soul <pushkarurode@gmail.com>
2026-08-18 17:31:42 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,6 @@ const app = express();
 
 app.use(cors({
   origin:[
-    "https://expense-tracker-pied-gamma-45.vercel.app",
-    "https://expense-tracker-server-flame.vercel.app",
     "http://localhost:5173"
   ],
   credentials: true
